47b9d44aa18d06736f9f6235ca9c4384ca5d7e32
Travsify <support@rentilly.ng>
2026-09-03 03:09:29 -0700

    fix: import foundation.dart in api_service.dart for debugPrint

--- mobile/lib/services/api_service.dart
+++ mobile/lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../constants/app_constants.dart';
 import '../models/property.dart';
