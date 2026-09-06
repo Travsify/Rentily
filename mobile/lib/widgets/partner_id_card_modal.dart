b1e6b6e1bfb19b9f58c5b6ca0df7a5d9896888e9
Travsify <support@rentilly.ng>
2026-09-06 05:42:14 -0700

    fix: add share_plus import in partner_id_card_modal

--- mobile/lib/widgets/partner_id_card_modal.dart
+++ mobile/lib/widgets/partner_id_card_modal.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
